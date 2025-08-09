3dbd2b225987b267d71cf1fad8b290612bf3e4bd
davidllr55 <david.llr@hotmail.com>
2025-08-10 01:13:57 +0200

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,7 +1,4 @@
-export const runtime = 'edge';
-
-export default function handler() {
-  return new Response(JSON.stringify({ ok: true, time: new Date().toISOString() }), {
-    headers: { 'content-type': 'application/json', 'access-control-allow-origin': '*' }
-  });
+// Serverless function de prueba
+export default async function handler(req, res) {
+  res.status(200).json({ ok: true, time: new Date().toISOString() });
 }
